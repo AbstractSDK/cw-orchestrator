22ee5d22a3eb5d42eaf77b2f7774e020415c97dc
CyberHoward <CyberHoward@protonmail.com>
2022-03-01 09:50:26 +0000

    update cargo

--- terra-rust-script-derive/src/lib.rs
+++ terra-rust-script-derive/src/lib.rs
@@ -1,17 +1,16 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use proc_macro2::{Span, TokenStream as TokenStream2, Ident};
+use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
 use syn::punctuated::Punctuated;
 use syn::token::Comma;
-use syn::{self, parse_macro_input, DeriveInput, Visibility, Field};
+use syn::{self, parse_macro_input, DeriveInput, Field, Visibility};
 
 use quote::{format_ident, quote, quote_spanned};
 use syn::spanned::Spanned;
 use syn::{Data, Error, Fields};
 // https://crates.io/crates/convert_case
 use convert_case::{Case, Casing};
-
 
 // cargo watch -q -c -x 'expand --lib macro_dev'
 
@@ -22,7 +21,7 @@ use convert_case::{Case, Casing};
 
 //     let token_stream2 = quote! {
 //         impl #name {
-//             pub fn test() -> String { 
+//             pub fn test() -> String {
 //                 format!("{:?}",#name)
 //             }
 //         }
@@ -55,7 +54,6 @@ pub fn derive_contract(item: TokenStream) -> TokenStream {
     match data {
         // Only if data is an enum, we do parsing
         Data::Enum(data_enum) => {
-
             // data_enum is of type syn::DataEnum
             // https://doc.servo.org/syn/struct.DataEnum.html
 
@@ -67,7 +65,6 @@ pub fn derive_contract(item: TokenStream) -> TokenStream {
             // https://doc.servo.org/syn/punctuated/struct.Punctuated.html
             // https://doc.servo.org/syn/struct.Variant.html
             for variant in &data_enum.variants {
-
                 // Variant's name
                 let ref variant_name = variant.ident;
 
@@ -78,21 +75,28 @@ pub fn derive_contract(item: TokenStream) -> TokenStream {
                     Fields::Unnamed(_) => quote_spanned! {variant.span()=> (..) },
                     Fields::Unit => quote_spanned! { variant.span()=> },
                     Fields::Named(fields_temp) => {
-
                         // construct an identifier named execute_<variant_name> for function name
                         // We convert it to snake case using `to_case(Case::Snake)`
                         let mut is_variant_func_name =
-                        format_ident!("{}", variant_name.to_string().to_case(Case::Snake));
+                            format_ident!("{}", variant_name.to_string().to_case(Case::Snake));
                         is_variant_func_name.set_span(variant_name.span());
 
                         let a = fields_temp.named.clone();
 
                         // todo: only remove attr if Attr is of type outer
-                        let b: Punctuated<Field, Comma> = a.iter().map(|field| {let mut temp = field.clone(); temp.vis = Visibility::Inherited;
-                            temp.attrs = vec![]; temp}).collect();
-                        
-                        let idents: Punctuated<Ident, Comma> = a.iter().map(|ident| ident.ident.clone().unwrap()).collect();
-                        
+                        let b: Punctuated<Field, Comma> = a
+                            .iter()
+                            .map(|field| {
+                                let mut temp = field.clone();
+                                temp.vis = Visibility::Inherited;
+                                temp.attrs = vec![];
+                                temp
+                            })
+                            .collect();
+
+                        let idents: Punctuated<Ident, Comma> =
+                            a.iter().map(|ident| ident.ident.clone().unwrap()).collect();
+
                         // Here we construct the function for the current variant
                         variant_checker_functions.extend(quote_spanned! {variant.span()=>
                             pub fn #is_variant_func_name(#b) -> Self {
@@ -106,35 +110,37 @@ pub fn derive_contract(item: TokenStream) -> TokenStream {
                     }
                 };
             }
-                let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
-                
-                let expanded = quote! {
-                    impl #impl_generics #name #ty_generics #where_clause {
-                        // variant_checker_functions gets replaced by all the functions
-                        // that were constructed above
-                        #variant_checker_functions
-                    }
-                };
-                return TokenStream::from(expanded)
-             
-            
+            let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+            let expanded = quote! {
+                impl #impl_generics #name #ty_generics #where_clause {
+                    // variant_checker_functions gets replaced by all the functions
+                    // that were constructed above
+                    #variant_checker_functions
+                }
+            };
+            return TokenStream::from(expanded);
         }
         Data::Struct(data_struct) => {
-
             let mut struct_constructor_function = TokenStream2::new();
             match &data_struct.fields {
                 Fields::Named(fields_temp) => {
-                    
-                    let is_variant_func_name =
-                    format_ident!("instantiate");
-                    
-                    let a = fields_temp.named.clone();
-                    let b: Punctuated<Field, Comma> = a.iter().map(|field| {let mut temp = field.clone(); temp.vis = Visibility::Inherited;
-                        temp.attrs = vec![]; temp}).collect();
+                    let is_variant_func_name = format_ident!("instantiate");
 
-                    let idents: Punctuated<Ident, Comma> = a.iter().map(|ident| ident.ident.clone().unwrap()).collect();
-                    
-                    
+                    let a = fields_temp.named.clone();
+                    let b: Punctuated<Field, Comma> = a
+                        .iter()
+                        .map(|field| {
+                            let mut temp = field.clone();
+                            temp.vis = Visibility::Inherited;
+                            temp.attrs = vec![];
+                            temp
+                        })
+                        .collect();
+
+                    let idents: Punctuated<Ident, Comma> =
+                        a.iter().map(|ident| ident.ident.clone().unwrap()).collect();
+
                     // Here we construct the function for the current variant
                     struct_constructor_function.extend(quote! {
                         pub fn #is_variant_func_name(#b) -> Self {
@@ -143,19 +149,18 @@ pub fn derive_contract(item: TokenStream) -> TokenStream {
                             }
                         }
                     });
-                    
-            },
-            _ => ()
-        }
-        let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
-                
-        let expanded = quote! {
-            impl #impl_generics #name #ty_generics #where_clause {
-                #struct_constructor_function
+                }
+                _ => (),
             }
-        };
-        return TokenStream::from(expanded)
-    },
+            let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+            let expanded = quote! {
+                impl #impl_generics #name #ty_generics #where_clause {
+                    #struct_constructor_function
+                }
+            };
+            return TokenStream::from(expanded);
+        }
         _ => return derive_error!("IsVariant is only implemented for enums"),
     }
 }
