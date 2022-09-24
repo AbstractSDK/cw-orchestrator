672b68ff4765f584eca73c98ac1a7c2b5aa6ca46
CyberHoward <CyberHoward@protonmail.com>
2022-09-24 18:37:55 +0000

    update deps

--- boot-core/src/lib.rs
+++ boot-core/src/lib.rs
@@ -33,24 +33,24 @@ extern crate lazy_static;
 use data_structures::{client_types, core_types};
 
 pub mod cosmos_modules {
-    pub use cosmos_sdk_proto::cosmos::auth::v1beta1 as auth;
-    pub use cosmos_sdk_proto::cosmos::authz::v1beta1 as authz;
-    pub use cosmos_sdk_proto::cosmos::bank::v1beta1 as bank;
-    pub use cosmos_sdk_proto::cosmos::base::abci::v1beta1 as abci;
-    pub use cosmos_sdk_proto::cosmos::base::tendermint::v1beta1 as tendermint;
-    pub use cosmos_sdk_proto::cosmos::base::v1beta1 as base;
-    pub use cosmos_sdk_proto::cosmos::crisis::v1beta1 as crisis;
-    pub use cosmos_sdk_proto::cosmos::distribution::v1beta1 as distribution;
-    pub use cosmos_sdk_proto::cosmos::evidence::v1beta1 as evidence;
-    pub use cosmos_sdk_proto::cosmos::feegrant::v1beta1 as feegrant;
-    pub use cosmos_sdk_proto::cosmos::gov::v1beta1 as gov;
-    pub use cosmos_sdk_proto::cosmos::mint::v1beta1 as mint;
-    pub use cosmos_sdk_proto::cosmos::params::v1beta1 as params;
-    pub use cosmos_sdk_proto::cosmos::slashing::v1beta1 as slashing;
-    pub use cosmos_sdk_proto::cosmos::staking::v1beta1 as staking;
-    pub use cosmos_sdk_proto::cosmos::tx::v1beta1 as tx;
-    pub use cosmos_sdk_proto::cosmwasm::wasm::v1 as cosmwasm;
-    pub use cosmos_sdk_proto::tendermint::abci as tendermint_abci;
+    pub use cosmrs::proto::cosmos::auth::v1beta1 as auth;
+    pub use cosmrs::proto::cosmos::authz::v1beta1 as authz;
+    pub use cosmrs::proto::cosmos::bank::v1beta1 as bank;
+    pub use cosmrs::proto::cosmos::base::abci::v1beta1 as abci;
+    pub use cosmrs::proto::cosmos::base::tendermint::v1beta1 as tendermint;
+    pub use cosmrs::proto::cosmos::base::v1beta1 as base;
+    pub use cosmrs::proto::cosmos::crisis::v1beta1 as crisis;
+    pub use cosmrs::proto::cosmos::distribution::v1beta1 as distribution;
+    pub use cosmrs::proto::cosmos::evidence::v1beta1 as evidence;
+    pub use cosmrs::proto::cosmos::feegrant::v1beta1 as feegrant;
+    pub use cosmrs::proto::cosmos::gov::v1beta1 as gov;
+    pub use cosmrs::proto::cosmos::mint::v1beta1 as mint;
+    pub use cosmrs::proto::cosmos::params::v1beta1 as params;
+    pub use cosmrs::proto::cosmos::slashing::v1beta1 as slashing;
+    pub use cosmrs::proto::cosmos::staking::v1beta1 as staking;
+    pub use cosmrs::proto::cosmos::tx::v1beta1 as tx;
+    pub use cosmrs::proto::cosmwasm::wasm::v1 as cosmwasm;
+    pub use cosmrs::proto::tendermint::abci as tendermint_abci;
 }
 
 // mod macro_dev {
