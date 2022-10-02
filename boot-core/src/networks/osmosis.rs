61176fb22119779377fc9440d14ef5dc7c0d0257
CyberHoward <CyberHoward@protonmail.com>
2022-10-02 09:51:03 -0500

    add osmo testnet

--- boot-core/src/networks/osmosis.rs
+++ boot-core/src/networks/osmosis.rs
@@ -9,16 +9,16 @@ pub const OSMO_CHAIN: ChainInfo = ChainInfo {
     coin_type: 118u32,
 };
 
-// pub const UNI_3: NetworkInfo = NetworkInfo {
-//     kind: NetworkKind::Testnet,
-//     id: "uni-3",
-//     gas_denom: "ujunox",
-//     gas_price: 0.025,
-//     grpc_url: "http://ssh.ohmroger.com:9099",
-//     chain_info: JUNO_CHAIN,
-//     lcd_url: None,
-//     fcd_url: None,
-// };
+pub const OSMO_4: NetworkInfo = NetworkInfo {
+    kind: NetworkKind::Testnet,
+    id: "osmo-test-4",
+    gas_denom: "uosmo",
+    gas_price: 0.025,
+    grpc_url: "https://grpc-test.osmosis.zone",
+    chain_info: OSMO_CHAIN,
+    lcd_url: None,
+    fcd_url: None,
+};
 
 // pub const JUNO_1: NetworkInfo = NetworkInfo {
 //     kind: NetworkKind::Mainnet,
