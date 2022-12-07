ff3562a7468dba56e84ee239cd0a736d9696a574
adairrr <32375605+adairrr@users.noreply.github.com>
2022-12-07 09:28:00 -0800

    Add non-owning set_mock method to Contract

--- boot-core/src/networks/mod.rs
+++ boot-core/src/networks/mod.rs
@@ -1,5 +1,5 @@
 mod juno;
 mod osmosis;
+pub use super::daemon::state::{ChainInfo, NetworkInfo, NetworkKind};
 pub use juno::{JUNO_1, LOCAL_JUNO, UNI_5};
 pub use osmosis::{LOCAL_OSMO, OSMO_4};
-pub use super::daemon::state::{ChainInfo,NetworkInfo,NetworkKind};
