e1f1ff9a3e60ab8a08fa17899aa42f7ba6fa7dd0
cyberhoward <cyberhoward@protonmail.com>
2022-12-20 12:06:31 +0100

    add init_balance method to mock

--- boot-core/src/mock/core.rs
+++ boot-core/src/mock/core.rs
@@ -40,6 +40,15 @@ pub struct Mock<S: StateInterface = MockState> {
 }
 
 impl<S: StateInterface> Mock<S> {
+    /// set the Bank balance of an address
+    pub fn init_balance(&self, address: &Addr, amount: Vec<cosmwasm_std::Coin>) -> Result<(), BootError> {
+        self.app.borrow_mut().init_modules(|router, _, storage| {
+            router.bank.init_balance(storage, address, amount)
+        }).map_err(Into::into)
+    }
+}
+
+impl<S: StateInterface> Mock<S> {
     pub fn new(
         sender: &Addr,
         state: &Rc<RefCell<S>>,
