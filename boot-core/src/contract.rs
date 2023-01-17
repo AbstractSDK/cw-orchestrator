47fe78d923d58f65b4876160bf4a862e8fe2c743
cyberhoward <cyberhoward@protonmail.com>
2023-01-17 16:13:48 +0100

    add `contract.id()` method

--- boot-core/src/contract.rs
+++ boot-core/src/contract.rs
@@ -108,6 +108,11 @@ impl<Chain: BootEnvironment + Clone> Contract<Chain> {
         }
     }
 
+    // Returns the contract id
+    pub fn id(&self) -> &str {
+        &self.id
+    }
+
     /// `get_chain` instead of `chain` to disambiguate from the std prelude .chain() method.
     pub fn get_chain(&self) -> &Chain {
         &self.chain
