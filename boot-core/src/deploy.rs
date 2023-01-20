754bdb89bddc3c6791bf464002f36a9260f48129
cyberhoward <cyberhoward@protonmail.com>
2023-01-20 15:54:38 +0100

    improve doc comments

--- boot-core/src/deploy.rs
+++ boot-core/src/deploy.rs
@@ -14,12 +14,18 @@ use crate::{BootError, BootEnvironment};
 /// impl Deploy for MyApplication<Mock> {
 ///     type Error = BootError;
 ///     type DeployData = Empty;
+///     // deploys the token to the chain
 ///     fn deploy_on(chain: Chain, data: Empty) -> Result<R, BootError> {
 ///         let token = Cw20::new("my-token", chain.clone());
 ///         self.token.upload()?;
 ///         self.token.instantiate(...)?;
 ///         Ok(Self { token })
 ///     }
+///    // loads the token from the chain
+///    fn load_from(chain: Chain) -> Result<R, BootError> {
+///         let token = Cw20::new("my-token", chain.clone());
+///         Ok(Self { token })
+///    }
 /// }
 /// ```
 /// 
@@ -32,5 +38,7 @@ pub trait Deploy<Chain: BootEnvironment>: Sized {
     /// Deploy the application to the chain.
     fn deploy_on(chain: Chain, data: Self::DeployData) -> Result<Self, Self::Error>;
     /// Load the application from the chain, assuming it has already been deployed.
+    /// This either loads contract addresses from the chain state manually or constructs the
+    /// boot contract wrappers that were used to deploy the application with the same name.
     fn load_from(chain: Chain) -> Result<Self, Self::Error>;
 }
