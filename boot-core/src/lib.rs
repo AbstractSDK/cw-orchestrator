506aab097b804b37d11ae7018668484683dd956d
cyberhoward <cyberhoward@protonmail.com>
2023-01-20 16:29:33 +0100

    expose Deploy in lib.rs

--- boot-core/src/lib.rs
+++ boot-core/src/lib.rs
@@ -24,6 +24,7 @@ pub use error::BootError;
 pub use index_response::IndexResponse;
 pub use mock::{core::Mock, state::MockState};
 pub use tx_handler::{TxHandler, TxResponse};
+pub use deploy::Deploy;
 /// Signals a supported execution environment
 pub trait BootEnvironment: TxHandler + Clone {}
 
