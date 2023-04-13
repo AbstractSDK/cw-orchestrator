23652213380f060d01f3291a1691d110a0d3de3f
Kayanski <kowalski.kowalskin@gmail.com>
2023-04-13 12:59:20 +0200

    Modified macro order

--- packages/boot-macros/src/lib.rs
+++ packages/boot-macros/src/lib.rs
@@ -13,8 +13,8 @@ pub fn boot_contract(_attrs: TokenStream, input: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(input as ItemFn);
 
     quote!{
-        #[cfg_attr(not(feature = "library"), ::cosmwasm_std::entry_point)]
         #[cfg_attr(feature="outside_contract", ::boot_contract_derive::boot_contract_raw)]
+        #[cfg_attr(not(feature = "library"), ::cosmwasm_std::entry_point)]
         #input_fn
     }.into()
 }
