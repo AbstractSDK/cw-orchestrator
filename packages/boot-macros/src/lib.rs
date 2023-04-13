4790f98bbe3a465fc8a8f5a8b74034ccf3c57ab1
Kayanski <kowalski.kowalskin@gmail.com>
2023-04-13 12:49:15 +0200

    Feature flagged the boot code

--- packages/boot-macros/src/lib.rs
+++ packages/boot-macros/src/lib.rs
@@ -10,13 +10,18 @@ use syn::parse_macro_input;
 #[proc_macro_attribute]
 pub fn boot_contract(_attrs: TokenStream, input: TokenStream) -> TokenStream {
 
+    let cloned = input.clone();
     // If the in_contract feature is enabled, we don't add the boot_contract macro
-    let input_fn = parse_macro_input!(input as ItemFn);
+    let input_fn = parse_macro_input!(cloned as ItemFn);
 
-    let boot_contract_macro = quote! {
-        #[::boot_contract_derive::boot_contract_raw]
-        #input_fn
-    };
+    #[cfg(feature="outside_contract")]
+    {
+        quote! {
+            #[::boot_contract_derive::boot_contract_raw]
+            #input_fn
+        }.into()
+    }
 
-    boot_contract_macro.into()
+    #[cfg(not(feature="outside_contract"))]
+    input
 }
