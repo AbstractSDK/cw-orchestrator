464db6d1c62b1fc7e2c94462becc74641360ad35
BHIKTOR <rottengzip@gmail.com>
2023-04-20 06:56:48 -0600

    review ready

--- boot-core/src/daemon/queriers/bank.rs
+++ boot-core/src/daemon/queriers/bank.rs
@@ -1,5 +1,5 @@
 use crate::{daemon::cosmos_modules, DaemonError};
-use cosmrs::tendermint::{Block, Time};
+use cosmrs::proto::cosmos::base::v1beta1::Coin;
 use tonic::transport::Channel;
 
 use super::DaemonQuerier;
@@ -16,41 +16,35 @@ impl DaemonQuerier for Bank {
 }
 
 impl Bank {
-    pub async fn latest_block(&self) -> Result<Block, DaemonError> {
-        let mut client =
-            cosmos_modules::tendermint::service_client::ServiceClient::new(self.channel.clone());
-        #[allow(deprecated)]
-        let resp = client
-            .get_latest_block(cosmos_modules::tendermint::GetLatestBlockRequest {})
-            .await?
-            .into_inner();
-        Ok(Block::try_from(resp.block.unwrap())?)
-    }
-
-    pub async fn block_height(&self) -> Result<u64, DaemonError> {
-        let block = self.latest_block().await?;
-        Ok(block.header.height.value())
-    }
-
-    /// Returns the block timestamp (since unix epoch) in nanos
-    pub async fn block_time(&self) -> Result<u128, DaemonError> {
-        let block = self.latest_block().await?;
-        Ok(block
-            .header
-            .time
-            .duration_since(Time::unix_epoch())?
-            .as_nanos())
-    }
-
-    pub async fn simulate_tx(&self, tx_bytes: Vec<u8>) -> Result<u64, DaemonError> {
-        let mut client =
-            cosmos_modules::tx::service_client::ServiceClient::new(self.channel.clone());
-        #[allow(deprecated)]
-        let resp = client
-            .simulate(cosmos_modules::tx::SimulateRequest { tx: None, tx_bytes })
-            .await?
-            .into_inner();
-        let gas_used = resp.gas_info.unwrap().gas_used;
-        Ok(gas_used)
+    /// Query the bank balance of a given address
+    /// If denom is None, returns all balances
+    pub async fn coin_balance(
+        &self,
+        address: impl Into<String>,
+        denom: Option<impl Into<String>>,
+    ) -> Result<Vec<Coin>, DaemonError> {
+        use cosmos_modules::bank::query_client::QueryClient;
+        match denom {
+            Some(denom) => {
+                let mut client: QueryClient<Channel> = QueryClient::new(self.channel.clone());
+                let request = cosmos_modules::bank::QueryBalanceRequest {
+                    address: address.into(),
+                    denom: denom.into(),
+                };
+                let resp = client.balance(request).await?.into_inner();
+                let coin = resp.balance.unwrap();
+                Ok(vec![coin.into()])
+            }
+            None => {
+                let mut client: QueryClient<Channel> = QueryClient::new(self.channel.clone());
+                let request = cosmos_modules::bank::QueryAllBalancesRequest {
+                    address: address.into(),
+                    ..Default::default()
+                };
+                let resp = client.all_balances(request).await?.into_inner();
+                let coins = resp.balances;
+                Ok(coins.into_iter().map(|c| c.into()).collect())
+            }
+        }
     }
 }
