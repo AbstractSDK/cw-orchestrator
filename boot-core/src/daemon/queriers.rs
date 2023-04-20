a5c06f7b9b498d4d6ebfdfbb7b715b3cad8df5d4
cyberhoward <cyberhoward@protonmail.com>
2023-04-20 16:53:15 +0200

    cosmos query macro construction

--- boot-core/src/daemon/queriers.rs
+++ boot-core/src/daemon/queriers.rs
@@ -1,9 +1,34 @@
+#[macro_export]
+macro_rules! cosmos_query {
+    ($self: ident, $module:ident, $request_type:ident { $($field:ident : $value:expr),* $(,)? }) => {{
+        use crate::daemon::cosmos_modules::$module::{
+            query_client::QueryClient, $request_type,
+        };
+        use paste::paste;
+
+        let mut client = QueryClient::new($self.channel.clone());
+        let request = $request_type { $($field : $value),* };
+
+        paste! {
+            client.[<$request_type:snake>](request).await?.into_inner()
+        }
+        ::log::debug!(
+            "cosmos_query: {:?} resulted in: {:?}",
+            request,
+            resp
+        );
+        resp
+    }};
+}
+
 mod bank;
 mod cosmwasm;
+mod ibc;
 mod node;
 
 pub use bank::Bank;
 pub use cosmwasm::CosmWasm;
+pub use ibc::Ibc;
 pub use node::Node;
 
 use tonic::transport::Channel;
