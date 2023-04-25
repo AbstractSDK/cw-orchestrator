217fbe3efcb81cd33d41affefd9cbfe4e148c20a
cyberhoward <cyberhoward@protonmail.com>
2023-04-25 18:29:59 +0200

    rename cw-plus-...

--- publish.sh
+++ publish.sh
@@ -16,7 +16,7 @@ fi
 # these are imported by other packages
 BASE_PACKAGES="cw-orc-contract-derive cw-orc-fns-derive"
 UTILS_PACKAGES="cw-orc"
-ALL_PACKAGES="cw-plus-orchestrate"
+ALL_PACKAGES="cw-plus-orc"
 
 for pack in $BASE_PACKAGES; do
   (
