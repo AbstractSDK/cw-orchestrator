2ca43ed6fa76d5109542f5940a2c11c9ec365695
CyberHoward <88450409+CyberHoward@users.noreply.github.com>
2023-04-28 15:22:34 +0200

    Throw error on failed tx (#106)
    * Add error when tx fails
    
    * remove Option from channel connect as it was unused
    
    * update find_tx_by_hash to err instead of panic
    
    * update channel creation uses
    
    * add a daemon constructor in tests
    
    * add initial sender tests

---
+++ cw-orch/tests/sender.rs
@@ -0,0 +1,65 @@
+mod common;
+
+use cosmwasm_std::{coin, Uint128};
+use cw_orch::{networks::LOCAL_JUNO, *};
+use speculoos::{
+    result::{ContainingResultAssertions, ResultAssertions},
+    *,
+};
+use tokio::runtime::Runtime;
+
+#[test]
+fn tx_not_found_after_x() {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let mut bad_config = LOCAL_JUNO;
+    bad_config.gas_denom = "baddenom";
+
+    let daemon = Daemon::builder()
+        .chain(bad_config)
+        .handle(rt.handle())
+        .build()
+        .unwrap();
+    let cw20 = common::contract::Cw20::new(daemon);
+
+    // attempt to execute with a wrong fee denom, hash won't be included in the block
+    let res = cw20.upload();
+
+    // we expect an error
+    asserting!("the transaction is not found")
+        .that(&res)
+        .is_err()
+        .matches(|e| e.to_string().contains("not found after"));
+}
+
+#[test]
+fn cosmwasm_exec_fails() {
+    let runtime = Runtime::new().unwrap();
+    let (sender, contract) = common::contract::start(&runtime);
+
+    // upload contract
+    let upload_res = contract.upload();
+    asserting!("upload is succesful").that(&upload_res).is_ok();
+
+    // init msg for contract
+    let init_msg = common::contract::get_init_msg(&sender);
+    contract
+        .instantiate(&init_msg, Some(&sender.clone()), None)
+        .unwrap();
+
+    // burn more than we have amount
+    let res = contract.execute(
+        &cw20_base::msg::ExecuteMsg::Burn {
+            amount: Uint128::from(1000000000000000000u128),
+        },
+        None,
+    );
+
+    // we expect an error
+    asserting!("contract execution failed")
+        .that(&res)
+        .is_err()
+        .matches(|e| match e {
+            CwOrcError::DaemonError(DaemonError::Status(_)) => true,
+            _ => false,
+        });
+}
