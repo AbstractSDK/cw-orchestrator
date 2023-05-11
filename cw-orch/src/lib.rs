3f6ba93c0b961c4cd21ac1d40ac51571c63bc0b3
CyberHoward <88450409+CyberHoward@users.noreply.github.com>
2023-05-11 18:00:03 +0300

    Refactor contract interface (#119)
    * interface to interface_entry_point
    
    * contract to interface
    
    * Re-export anyhow
    
    * Fix test
    
    * bump pre-release versions
    
    * Update macros for use in contracts
    
    * format tmol
    
    ---------
    
    Co-authored-by: adairrr <32375605+adairrr@users.noreply.github.com>

--- cw-orch/src/lib.rs
+++ cw-orch/src/lib.rs
@@ -1,14 +1,18 @@
 #![doc(html_logo_url = "https://raw.githubusercontent.com/AbstractSDK/assets/mainline/logo.svg")]
 // macros
-pub use cw_orch_contract_derive::{contract, interface};
+pub use cw_orch_contract_derive::{interface, interface_entry_point};
 pub use cw_orch_fns_derive::{ExecuteFns, QueryFns};
+
+// Re-export anyhow for the macro
+pub extern crate anyhow;
+
 // prelude
 pub mod prelude;
 
 pub mod contract;
 #[cfg(feature = "daemon")]
 pub mod daemon;
-mod deploy;
+pub mod deploy;
 pub mod environment;
 mod error;
 mod index_response;
