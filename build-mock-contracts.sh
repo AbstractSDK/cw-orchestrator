0b70a476d807c36fcee2b6bb157c9c3b04967ff5
cyberhoward <cyberhoward@protonmail.com>
2023-05-16 17:19:26 +0300

    make build script arch generic

--- build-mock-contracts.sh
+++ build-mock-contracts.sh
@@ -1,5 +1,13 @@
-#cargo wasm
-sudo docker run --rm -v "$(pwd)":/code \
+#!/usr/bin/env bash
+
+if [[ $(arch) == "arm64" ]]; then
+  image="cosmwasm/workspace-optimizer-arm64"
+else
+  image="cosmwasm/workspace-optimizer"
+fi
+
+# Optimized builds
+docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.12.13
+  ${image}:0.12.13
