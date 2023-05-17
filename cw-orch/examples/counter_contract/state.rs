a5b4a37fb74b4281d665250d31381438d0ae8164
BHIKTOR <rottengzip@gmail.com>
2023-05-17 17:53:30 -0600

    fixing folders

--- cw-orch/examples/counter_contract/state.rs
+++ cw-orch/examples/counter_contract/state.rs
@@ -1,13 +1,11 @@
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
-use cosmwasm_std::Addr;
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Uint128;
 use cw_storage_plus::Item;
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
-}
+// we use the cw_serde macro to add different traits related to our struct
+// that will help us work with it in general
+#[cw_serde]
+pub struct Count(pub Uint128);
 
-pub const STATE: Item<State> = Item::new("state");
+// this will hold our data!
+pub const COUNT: Item<Count> = Item::new("count");
