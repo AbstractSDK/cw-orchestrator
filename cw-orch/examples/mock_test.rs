4716eda0d81657a80f5df62cc45867c708059115
BHIKTOR <rottengzip@gmail.com>
2023-05-18 10:11:26 -0600

    minor changes to the tests so they are actual tests and fixed the daemon one to actually work

--- cw-orch/examples/mock_test.rs
+++ cw-orch/examples/mock_test.rs
@@ -8,29 +8,21 @@ mod counter_contract;
 
 pub fn main() {
     let sender = Addr::unchecked("juno16g2rahf5846rxzp3fwlswy08fz8ccuwk03k57y");
-    // let chain = networks::parse_network(&env::var("CHAIN").unwrap());
 
     let mock = Mock::new(&sender).unwrap();
 
     let contract_counter =
         contract_counter::contract::ContractCounter::new("mock:contract_counter", mock);
 
-    let upload_res = contract_counter.upload().unwrap();
-    println!("upload_res: {:#?}", upload_res);
+    let upload_res = contract_counter.upload();
+    assert!(upload_res.is_ok());
 
-    let init_res = contract_counter
-        .instantiate(&InstantiateMsg { count: 0 }, Some(&sender), None)
-        .unwrap();
-    println!("init_res: {:#?}", init_res);
+    let init_res = contract_counter.instantiate(&InstantiateMsg { count: 0 }, Some(&sender), None);
+    assert!(init_res.is_ok());
 
-    let exec_res = contract_counter
-        .execute(&ExecuteMsg::Increment {}, None)
-        .unwrap();
-    println!("exec_res: {:#?}", exec_res);
+    let exec_res = contract_counter.execute(&ExecuteMsg::Increment {}, None);
+    assert!(exec_res.is_ok());
 
-    let query_res = contract_counter
-        .query::<GetCountResponse>(&QueryMsg::GetCount {})
-        .unwrap();
-
-    println!("query_res: {:#?}", query_res);
+    let query_res = contract_counter.query::<GetCountResponse>(&QueryMsg::GetCount {});
+    assert!(query_res.is_ok());
 }
