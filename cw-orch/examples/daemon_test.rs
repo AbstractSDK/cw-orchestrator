496ae354d8432b1f8dc22b27c8327c5593be1d43
BHIKTOR <rottengzip@gmail.com>
2023-05-18 10:11:26 -0600

    minor changes to the tests so they are actual tests and fixed the daemon one to actually work

--- cw-orch/examples/daemon_test.rs
+++ cw-orch/examples/daemon_test.rs
@@ -10,13 +10,15 @@ use tokio::runtime::Runtime;
 extern crate cw_orch;
 mod counter_contract;
 
+const LOCAL_MNEMONIC: &str = "clip hire initial neck maid actor venue client foam budget lock catalog sweet steak waste crater broccoli pipe steak sister coyote moment obvious choose";
+
 pub fn main() {
     let runtime = Runtime::new().unwrap();
 
     let res = Daemon::builder()
-        .chain(networks::parse_network(&env::var("CHAIN").unwrap()))
+        .chain(networks::LOCAL_JUNO)
         .handle(runtime.handle())
-        .mnemonic(env::var("LOCAL_MNEMONIC").unwrap())
+        .mnemonic(LOCAL_MNEMONIC)
         .build();
 
     let Some(daemon) = res.as_ref().ok() else {
@@ -26,26 +28,19 @@ pub fn main() {
     let contract_counter =
         contract_counter::contract::ContractCounter::new("local:contract_counter", daemon.clone());
 
-    let upload_res = contract_counter.upload().unwrap();
-    println!("upload_res: {:#?}", upload_res);
+    let upload_res = contract_counter.upload();
+    assert!(upload_res.is_ok());
 
-    let init_res = contract_counter
-        .instantiate(
-            &InstantiateMsg { count: 0 },
-            Some(&contract_counter.get_chain().sender()),
-            None,
-        )
-        .unwrap();
-    println!("init_res: {:#?}", init_res);
+    let init_res = contract_counter.instantiate(
+        &InstantiateMsg { count: 0 },
+        Some(&contract_counter.get_chain().sender()),
+        None,
+    );
+    assert!(init_res.is_ok());
 
-    let exec_res = contract_counter
-        .execute(&ExecuteMsg::Increment {}, None)
-        .unwrap();
-    println!("exec_res: {:#?}", exec_res);
+    let exec_res = contract_counter.execute(&ExecuteMsg::Increment {}, None);
+    assert!(exec_res.is_ok());
 
-    let query_res = contract_counter
-        .query::<GetCountResponse>(&QueryMsg::GetCount {})
-        .unwrap();
-
-    println!("query_res: {:#?}", query_res);
+    let query_res = contract_counter.query::<GetCountResponse>(&QueryMsg::GetCount {});
+    assert!(query_res.is_ok());
 }
