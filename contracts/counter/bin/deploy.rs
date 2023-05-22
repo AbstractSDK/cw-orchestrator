65ca4e0292996277c7cc2f32527e8316917f2c8c
cyberhoward <cyberhoward@protonmail.com>
2023-05-22 22:28:54 +0300

    remove bin folder from gitignore

---
+++ contracts/counter/bin/deploy.rs
@@ -0,0 +1,24 @@
+use counter_contract::msg::InstantiateMsg;
+use counter_contract::CounterContract;
+use cw_orch::prelude::*;
+use cw_orch::tokio;
+use tokio::runtime::Runtime;
+
+/// Script that registers the first Account in abstract (our Account)
+pub fn main() -> anyhow::Result<()> {
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    let rt = Runtime::new()?;
+    let network = networks::LOCAL_JUNO;
+    let chain = DaemonBuilder::default()
+        .handle(rt.handle())
+        .chain(network)
+        .build()?;
+
+    let counter = CounterContract::new("counter_contract", chain);
+
+    counter.upload()?;
+    counter.instantiate(&InstantiateMsg { count: 0 }, None, None)?;
+    Ok(())
+}
