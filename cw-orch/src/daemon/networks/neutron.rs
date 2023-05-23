7f8bd9873d103ca0768fa9df976b4d4daaea7966
Maurits Bos <35366788+MbBrainz@users.noreply.github.com>
2023-05-23 15:33:56 +0200

    Update neutron.rs with pion1 and neutron1 (#136)
    * Update neutron.rs with pion1 and neutron1
    
    * Update mod.rs
    
    * updated mod.rs to take PION_1 and NEUTRON_1

--- cw-orch/src/daemon/networks/neutron.rs
+++ cw-orch/src/daemon/networks/neutron.rs
@@ -6,14 +6,26 @@ pub const NEUTRON_NETWORK: NetworkInfo = NetworkInfo {
     coin_type: 118u32,
 };
 
-/// <https://github.com/neutron-org/cosmos-testnets/tree/master/replicated-security/baryon-1>
-pub const BARYON_1: ChainInfo = ChainInfo {
+/// <https://github.com/cosmos/chain-registry/blob/master/testnets/neutrontestnet/chain.json>
+pub const PION_1: ChainInfo = ChainInfo {
     kind: ChainKind::Testnet,
-    chain_id: "baryon-1",
+    chain_id: "pion-1",
     gas_denom: "untrn",
     gas_price: 0.001,
-    grpc_urls: &["http://grpc.baryon.ntrn.info:80"],
+    grpc_urls: &["http://grpc-palvus.pion-1.ntrn.tech:80"],
     network_info: NEUTRON_NETWORK,
-    lcd_url: Some("https://rest.baryon.ntrn.info"),
+    lcd_url: Some("https://rest-palvus.pion-1.ntrn.tech"),
+    fcd_url: None,
+};
+
+/// <https://github.com/cosmos/chain-registry/blob/master/neutron/chain.json>
+pub const NEUTRON_1: ChainInfo = ChainInfo {
+    kind: ChainKind::Mainnet,
+    chain_id: "neutron-1",
+    gas_denom: "untrn",
+    gas_price: 0.001,
+    grpc_urls: &["grpc-kralum.neutron-1.neutron.org:80"],
+    network_info: NEUTRON_NETWORK,
+    lcd_url: Some("https://rest-kralum.neutron-1.neutron.org"),
     fcd_url: None,
 };
