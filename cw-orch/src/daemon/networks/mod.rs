38633609127407c1f58cf055e9c49572de572065
Maurits Bos <35366788+MbBrainz@users.noreply.github.com>
2023-05-23 14:22:46 +0200

    Update mod.rs

--- cw-orch/src/daemon/networks/mod.rs
+++ cw-orch/src/daemon/networks/mod.rs
@@ -14,7 +14,7 @@ pub use archway::CONSTANTINE_1;
 pub use injective::{INJECTIVE_1, INJECTIVE_888};
 pub use juno::{JUNO_1, LOCAL_JUNO, UNI_6};
 pub use kujira::HARPOON_4;
-pub use neutron::BARYON_1;
+pub use neutron::{NEUTRON_1, PION_1};
 pub use osmosis::{LOCAL_OSMO, OSMO_4};
 pub use terra::{LOCAL_TERRA, PHOENIX_1, PISCO_1};
 
