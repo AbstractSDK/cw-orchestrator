0cc898210cbb05671370a63e3c3f6a416740e880
adairrr <32375605+adairrr@users.noreply.github.com>
2023-07-24 16:47:59 +0200

    Add archway constantine-3 and archway-1 networks

--- cw-orch/src/daemon/networks/archway.rs
+++ cw-orch/src/daemon/networks/archway.rs
@@ -6,7 +6,10 @@ pub const ARCHWAY_NETWORK: NetworkInfo = NetworkInfo {
     coin_type: 118u32,
 };
 
-/// <https://docs.archway.io/docs/overview/network>
+#[deprecated(
+    since = "0.6.1",
+    note = "Constantine-1 does not exist anymore. Use Constantine-3 instead."
+)]
 pub const CONSTANTINE_1: ChainInfo = ChainInfo {
     kind: ChainKind::Testnet,
     chain_id: "constantine-1",
@@ -15,5 +18,30 @@ pub const CONSTANTINE_1: ChainInfo = ChainInfo {
     grpc_urls: &["https://grpc.constantine-1.archway.tech:443"],
     network_info: ARCHWAY_NETWORK,
     lcd_url: Some("https://api.constantine-1.archway.tech"),
+    fcd_url: None,
+};
+
+/// <https://docs.archway.io/resources/networks>
+pub const CONSTANTINE_3: ChainInfo = ChainInfo {
+    kind: ChainKind::Testnet,
+    chain_id: "constantine-3",
+    gas_denom: "uconst",
+    gas_price: 0.025,
+    grpc_urls: &["https://grpc.constantine.archway.tech:443"],
+    network_info: ARCHWAY_NETWORK,
+    lcd_url: Some("https://api.constantine.archway.tech"),
+    fcd_url: None,
+};
+
+
+/// <https://docs.archway.io/resources/networks>
+pub const ARCHWAY_1: ChainInfo = ChainInfo {
+    kind: ChainKind::Mainnet,
+    chain_id: "archway-1",
+    gas_denom: "uarch",
+    gas_price: 0.025,
+    grpc_urls: &["https://grpc.mainnet.archway.io:443"],
+    network_info: ARCHWAY_NETWORK,
+    lcd_url: Some("https://api.mainnet.archway.io"),
     fcd_url: None,
 };
