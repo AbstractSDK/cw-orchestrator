23867e4d464a7cfbed62cfff3758c28b3adb8c9c
Kayanski <kowalski.kowalskin@gmail.com>
2023-09-03 19:02:05 +0200

    Added test for argument order

--- contracts/mock_contract/src/msg_tests.rs
+++ contracts/mock_contract/src/msg_tests.rs
@@ -2,37 +2,93 @@
 // This is a contract not exposed, only used for compilation test (necessary)
 
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Addr, Empty};
+use cosmwasm_std::{Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult};
 use cw_orch::prelude::*;
 
 #[cw_serde]
-#[cfg_attr(feature = "interface", derive(cw_orch::ExecuteFns))]
-#[cfg_attr(feature = "interface", disable_fields_sorting)]
+#[derive(cw_orch::ExecuteFns)]
+#[disable_fields_sorting]
 pub enum ExecuteMsg {
     Test { b: u64, a: String },
 }
 
 #[cw_serde]
-#[cfg_attr(feature = "interface", derive(cw_orch::ExecuteFns))]
+#[derive(cw_orch::ExecuteFns)]
 pub enum ExecuteMsgOrdered {
     Test { b: u64, a: String },
 }
 
 #[cw_orch::interface(Empty, ExecuteMsg, Empty, Empty)]
-pub struct MockContract;
+pub struct TestContract;
 
 #[cw_orch::interface(Empty, ExecuteMsgOrdered, Empty, Empty)]
-pub struct OrderedMockContract;
+pub struct OrderedTestContract;
 
-#[allow(unused)]
-pub fn test() -> Result<(), CwOrchError> {
-    let chain = Mock::new(&Addr::unchecked("sender"));
+pub fn instantiate(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: Empty,
+) -> StdResult<Response> {
+    Ok(Response::new())
+}
 
-    let contract = MockContract::new("test", chain.clone());
-    let contract_ordered = OrderedMockContract::new("test-ordered", chain.clone());
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: ExecuteMsg,
+) -> StdResult<Response> {
+    Ok(Response::new())
+}
 
-    contract.test(5, "test".to_string())?;
-    contract_ordered.test("test".to_string(), 5)?;
+pub fn execute_ordered(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: ExecuteMsgOrdered,
+) -> StdResult<Response> {
+    Ok(Response::new())
+}
 
-    Ok(())
+pub fn query(_deps: Deps, _env: Env, _msg: Empty) -> StdResult<Binary> {
+    Ok(vec![].into())
+}
+
+impl<Chain: CwEnv> Uploadable for TestContract<Chain> {
+    fn wrapper(&self) -> <Mock as TxHandler>::ContractSource {
+        Box::new(ContractWrapper::new_with_empty(execute, instantiate, query))
+    }
+}
+
+impl<Chain: CwEnv> Uploadable for OrderedTestContract<Chain> {
+    fn wrapper(&self) -> <Mock as TxHandler>::ContractSource {
+        Box::new(ContractWrapper::new_with_empty(
+            execute_ordered,
+            instantiate,
+            query,
+        ))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    pub fn test() -> Result<(), CwOrchError> {
+        let chain = Mock::new(&Addr::unchecked("sender"));
+
+        let contract = TestContract::new("test", chain.clone());
+        let contract_ordered = OrderedTestContract::new("test-ordered", chain.clone());
+
+        contract.upload()?;
+        contract_ordered.upload()?;
+        contract.instantiate(&Empty {}, None, None)?;
+        contract_ordered.instantiate(&Empty {}, None, None)?;
+
+        contract.test(5, "test".to_string())?;
+        contract_ordered.test("test".to_string(), 5)?;
+
+        Ok(())
+    }
 }
