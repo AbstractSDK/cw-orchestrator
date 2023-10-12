7380721809960147ee56ad19a05f46f08435cc38
Kayanski <44806566+Kayanski@users.noreply.github.com>
2023-10-12 18:43:11 +0300

    Changed conditional implementation for daemon + mock (#251)
    * Changed conditional impl
    
    * Added mock tests
    
    * Format
    
    * Small nits + comments

--- packages/cw-orch-core/src/environment/mod.rs
+++ packages/cw-orch-core/src/environment/mod.rs
@@ -2,6 +2,6 @@ mod cosmwasm_environment;
 mod index_response;
 mod state;
 
-pub use cosmwasm_environment::{CwEnv, TxHandler, TxResponse};
+pub use cosmwasm_environment::{CwEnv, TxHandler, TxResponse, WasmCodeQuerier};
 pub use index_response::IndexResponse;
 pub use state::{ChainState, DeployDetails, StateInterface};
