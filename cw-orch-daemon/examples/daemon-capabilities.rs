8ee47da6af54a52fad1d036b352f626b8af3defa
Kayanski <kowalski.kowalskin@gmail.com>
2023-10-23 08:18:46 +0200

    Added non-corking example

--- cw-orch-daemon/examples/daemon-capabilities.rs
+++ cw-orch-daemon/examples/daemon-capabilities.rs
@@ -3,16 +3,18 @@ use std::str::FromStr;
 use cosmrs::{tx::Msg, AccountId, Coin, Denom};
 use cosmwasm_std::coins;
 // ANCHOR: full_counter_example
-use cw_orch_daemon::DaemonBuilder;
+use cw_orch_daemon::{DaemonBuilder, parse_network_all};
 use cw_orch_networks::networks;
 use tokio::runtime::Runtime;
 
 const LOCAL_MNEMONIC: &str = "clip hire initial neck maid actor venue client foam budget lock catalog sweet steak waste crater broccoli pipe steak sister coyote moment obvious choose";
 pub fn main() -> anyhow::Result<()> {
+    env_logger::init();
     std::env::set_var("LOCAL_MNEMONIC", LOCAL_MNEMONIC);
 
     let rt = Runtime::new()?;
-    let network = networks::LOCAL_JUNO;
+    // let network = networks::LOCAL_JUNO;
+    let network = parse_network_all(rt.handle(), "doravota-1").unwrap();
     let daemon = DaemonBuilder::default()
         .handle(rt.handle())
         .chain(network)
