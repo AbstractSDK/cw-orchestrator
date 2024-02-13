65c222457ee948b8c6d0e7dfd4896ca986c04c7b
Kayanski <kowalski.kowalskin@gmail.com>
2024-02-13 09:08:58 +0100

    Re-export MockBase

--- packages/cw-orch-mock/src/lib.rs
+++ packages/cw-orch-mock/src/lib.rs
@@ -16,6 +16,10 @@ pub use self::core::{Mock, MockBech32};
 pub type MockApp = self::core::MockApp<MockApi>;
 pub type MockAppBech32 = self::core::MockApp<MockApiBech32>;
 
+pub mod base {
+    pub use super::core::MockBase;
+}
+
 use cosmwasm_std::testing::MockApi;
 use cw_multi_test::addons::MockApiBech32;
 pub use state::MockState;
