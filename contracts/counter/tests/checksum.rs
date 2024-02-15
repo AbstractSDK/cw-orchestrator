6a4f9d2040b3b023a415a1c37d27c13fcdc87eb6
Buckram <buckram123@gmail.com>
2024-02-15 13:34:10 +0200

    clippy warnings

--- contracts/counter/tests/checksum.rs
+++ contracts/counter/tests/checksum.rs
@@ -8,23 +8,21 @@ fn checksum() {
     use std::path::Path;
 
     let path = Path::new("../../artifacts/checksums.txt");
-    let file = File::open(&path).unwrap();
+    let file = File::open(path).unwrap();
     let lines = io::BufReader::new(file).lines();
     let mut found = false;
 
-    for line in lines {
-        if let Ok(ip) = line {
-            if ip.contains("counter_contract.wasm") {
-                let parts: Vec<&str> = ip.split_whitespace().collect();
-                if parts.len() > 1 {
-                    let calculated_hash = CounterContract::new(Mock::new("sender"))
-                        .wasm()
-                        .checksum()
-                        .unwrap();
-                    assert_eq!(parts[0], calculated_hash.to_string());
-                    found = true;
-                    break;
-                }
+    for line in lines.flatten() {
+        if line.contains("counter_contract.wasm") {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            if parts.len() > 1 {
+                let calculated_hash = CounterContract::new(Mock::new("sender"))
+                    .wasm()
+                    .checksum()
+                    .unwrap();
+                assert_eq!(parts[0], calculated_hash.to_string());
+                found = true;
+                break;
             }
         }
     }
