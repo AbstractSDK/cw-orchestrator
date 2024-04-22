7d4e19fa13baf0eb882b60207f0c7b318eaf8f39
Buckram <buckram123@gmail.com>
2024-04-22 11:41:46 +0300

    update comment for global write state

--- cw-orch-daemon/src/state.rs
+++ cw-orch-daemon/src/state.rs
@@ -15,7 +15,7 @@ use serde_json::{json, Value};
 use std::{collections::HashMap, path::Path, sync::Mutex};
 use tonic::transport::Channel;
 
-/// Global state for writing json
+/// Global state for sharing json files between daemons
 /// Key: path to state file
 /// Value: (DaemonState count that points to that state file, Json file)
 pub(crate) static GLOBAL_WRITE_STATE: Lazy<Mutex<HashMap<String, (u64, JsonFileState)>>> =
