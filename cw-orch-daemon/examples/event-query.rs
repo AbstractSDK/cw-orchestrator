785bfe0506f58804bbf124d4dd1c29f25f43fc8f
Kayanski <kowalski.kowalskin@gmail.com>
2024-04-25 09:53:38 +0000

    Added mnemonic comment

--- cw-orch-daemon/examples/event-query.rs
+++ cw-orch-daemon/examples/event-query.rs
@@ -3,6 +3,7 @@ use cw_orch_core::environment::NodeQuerier;
 use cw_orch_daemon::DaemonBuilder;
 use cw_orch_networks::networks;
 
+// From https://github.com/CosmosContracts/juno/blob/32568dba828ff7783aea8cb5bb4b8b5832888255/docker/test-user.env#L2
 const LOCAL_MNEMONIC: &str = "clip hire initial neck maid actor venue client foam budget lock catalog sweet steak waste crater broccoli pipe steak sister coyote moment obvious choose";
 const TX_HASH: &str = "501DB67945FCBDFF3248F1C18A721E5D107D862A8F77045D88FF2ECC7924C65A";
 pub fn main() -> anyhow::Result<()> {
