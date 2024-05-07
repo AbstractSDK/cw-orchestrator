e36f825fc49983874275ca35e1da4ccc1aefea77
Kayanski <kowalski.kowalskin@gmail.com>
2024-05-07 08:52:39 +0000

    fmt

--- packages/cw-orch-osmosis-test-tube/src/queriers/bank.rs
+++ packages/cw-orch-osmosis-test-tube/src/queriers/bank.rs
@@ -1,9 +1,10 @@
 use std::{cell::RefCell, rc::Rc};
 
 use cosmwasm_std::coin;
-use cw_orch::{environment::{
-    BankQuerier, Querier, QuerierGetter, StateInterface
-}, prelude::CwOrchError};
+use cw_orch::{
+    environment::{BankQuerier, Querier, QuerierGetter, StateInterface},
+    prelude::CwOrchError,
+};
 use osmosis_test_tube::osmosis_std::try_proto_to_cosmwasm_coins;
 use osmosis_test_tube::osmosis_std::types::cosmos::bank::v1beta1::{
     QuerySupplyOfRequest, QuerySupplyOfResponse,
