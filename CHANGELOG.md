6da91c16dca55d2c11fd22b33b3ef1c11f34c934
Kayanski <kowalski.kowalskin@gmail.com>
2024-05-14 14:55:34 +0000

    No panic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,8 @@
 
 - Added a test to make sure the derive macros stay compatible with new cw-orch versions
 - Changed the derive macros import from cw_orch to cw_orch_core. This allows changing the cw-orch API without breaking the derive macros.
-
+- Cw-orch mock env info doesn't error when using chain ids that don't match the `osmosis-1` pattern
+- 
 ## 0.22.0
 
 - Updated osmosis test tube to 24.0.1 ,that avoids re-compiling osmosis test tube
--- packages/cw-orch-mock/src/queriers/env.rs
+++ packages/cw-orch-mock/src/queriers/env.rs
@@ -9,10 +9,20 @@ impl<A: Api, S: StateInterface> EnvironmentQuerier for MockBase<A, S> {
     fn env_info(&self) -> EnvironmentInfo {
         let block_info = self.block_info().unwrap();
         let chain_id = block_info.chain_id.clone();
-        let chain_name = chain_id.rsplitn(2, '-').collect::<Vec<_>>()[1].to_string();
+        let chain_id_split = chain_id
+            .rsplitn(2, '-')
+            .map(ToString::to_string)
+            .collect::<Vec<_>>();
+
+        // We make sure this doesn't error even if the chain id doesn't have the `osmosis-1` format
+        let chain_name = if chain_id_split.len() > 2 {
+            &chain_id_split[1]
+        } else {
+            &chain_id_split[0]
+        };
         EnvironmentInfo {
             chain_id,
-            chain_name,
+            chain_name: chain_name.clone(),
             deployment_id: "default".to_string(),
         }
     }
