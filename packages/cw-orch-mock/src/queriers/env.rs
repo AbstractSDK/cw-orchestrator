73467888bff082f4b5022f693421eeccabd6d3f1
Kayanski <kowalski.kowalskin@gmail.com>
2024-05-15 11:34:08 +0000

    Applied nit

--- packages/cw-orch-mock/src/queriers/env.rs
+++ packages/cw-orch-mock/src/queriers/env.rs
@@ -9,17 +9,8 @@ impl<A: Api, S: StateInterface> EnvironmentQuerier for MockBase<A, S> {
     fn env_info(&self) -> EnvironmentInfo {
         let block_info = self.block_info().unwrap();
         let chain_id = block_info.chain_id.clone();
-        let chain_id_split = chain_id
-            .rsplitn(2, '-')
-            .map(ToString::to_string)
-            .collect::<Vec<_>>();
+        let chain_name = chain_id.rsplitn(2, '-').last().unwrap().to_string();
 
-        // We make sure this doesn't error even if the chain id doesn't have the `osmosis-1` format
-        let chain_name = if chain_id_split.len() > 1 {
-            &chain_id_split[1]
-        } else {
-            &chain_id_split[0]
-        };
         EnvironmentInfo {
             chain_id,
             chain_name: chain_name.clone(),
