27d515c41439138834453bfa0cb2f7b34bf21234
Kayanski <kowalski.kowalskin@gmail.com>
2024-06-17 16:24:56 +0000

    Re-export

--- packages/cw-orch-traits/src/lib.rs
+++ packages/cw-orch-traits/src/lib.rs
@@ -3,7 +3,9 @@ use cw_orch_core::environment::CwEnv;
 pub mod environment;
 pub mod stargate;
 
+pub use environment::Environment;
 pub use stargate::Stargate;
+
 pub trait FullNode: CwEnv + Stargate {}
 
 impl<C: CwEnv + Stargate> FullNode for C {}
