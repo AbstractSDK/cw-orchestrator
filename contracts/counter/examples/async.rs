ae3f8eceeb7c844daa05af56a4c49900171ef9b7
Buckram <buckram123@gmail.com>
2024-06-24 15:14:20 +0300

    Add reference of LOCAL_MNEMONIC

--- contracts/counter/examples/async.rs
+++ contracts/counter/examples/async.rs
@@ -3,6 +3,7 @@ use counter_contract::AsyncCounterQueryMsgFns;
 use counter_contract::CounterContract;
 use cw_orch::{anyhow, prelude::*, tokio};
 
+// From https://github.com/CosmosContracts/juno/blob/32568dba828ff7783aea8cb5bb4b8b5832888255/docker/test-user.env#L2
 const LOCAL_MNEMONIC: &str = "clip hire initial neck maid actor venue client foam budget lock catalog sweet steak waste crater broccoli pipe steak sister coyote moment obvious choose";
 
 #[tokio::main]
