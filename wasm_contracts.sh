f0dafb21d316996e9dd9a62e5cd7f70a13f24f85
Mykhailo Donchenko <91957742+Buckram123@users.noreply.github.com>
2024-07-29 16:46:44 +0300

    Bump optimizer (#456)

--- wasm_contracts.sh
+++ wasm_contracts.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ $(arch) == "arm64" ]]; then
-    image="cosmwasm/workspace-optimizer-arm64"
+    image="cosmwasm/optimizer-arm64:0.16.0"
 else
-    image="cosmwasm/workspace-optimizer"
+    image="cosmwasm/optimizer:0.16.0"
 fi
 
 # Optimized builds
 docker run --rm -v "$(pwd)":/code \
 --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
 --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-${image}:0.15.1
+${image}
