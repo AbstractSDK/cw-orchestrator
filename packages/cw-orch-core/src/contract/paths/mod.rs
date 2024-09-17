b491f0f8331e1d1dd370e04699687a02bfb1b982
Kayanski <kowalski.kowalskin@gmail.com>
2024-09-17 13:07:26 +0000

    Less diff

--- packages/cw-orch-core/src/contract/paths/mod.rs
+++
@@ -1,8 +0,0 @@
-pub use artifacts_dir::from_workspace;
-pub use artifacts_dir::ArtifactsDir;
-pub use github::{GithubWasmPath, GithubWasmPathLocation};
-pub use wasm_path::WasmPath;
-
-mod artifacts_dir;
-mod github;
-mod wasm_path;
