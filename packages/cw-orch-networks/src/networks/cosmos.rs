3a966e2e348034bf82d6df3d460d2bc94848b987
adairrr <32375605+adairrr@users.noreply.github.com>
2024-10-07 14:16:47 -0400

    Update to provider testnet

--- packages/cw-orch-networks/src/networks/cosmos.rs
+++ packages/cw-orch-networks/src/networks/cosmos.rs
@@ -7,18 +7,19 @@ pub const COSMOS_HUB_NETWORK: NetworkInfo = NetworkInfo {
     coin_type: 118,
 };
 
-pub const COSMOS_HUB_TESTNET: ChainInfo = THETA_TESTNET_001;
+pub const COSMOS_HUB_TESTNET: ChainInfo = ICS_TESTNET;
 
 /// Cosmos Hub Testnet
-/// @see https://github.com/cosmos/testnets/tree/master/release
-pub const THETA_TESTNET_001: ChainInfo = ChainInfo {
+/// @see https://github.com/cosmos/testnets/blob/master/interchain-security/provider/README.md
+/// Use the faucet here: https://faucet.polypore.xyz
+pub const ICS_TESTNET: ChainInfo = ChainInfo {
     kind: ChainKind::Testnet,
-    chain_id: "theta-testnet-001",
+    chain_id: "provider",
     gas_denom: "uatom",
     gas_price: 0.0025,
-    grpc_urls: &["https://grpc-t.cosmos.nodestake.top:443"],
+    grpc_urls: &["https://grpc-rs.cosmos.nodestake.top:443"],
     network_info: COSMOS_HUB_NETWORK,
-    lcd_url: Some("https://api-t.cosmos.nodestake.top:443"),
+    lcd_url: Some("https://api-rs.cosmos.nodestake.top:443"),
     fcd_url: None,
 };
 
