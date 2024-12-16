158e4f88f0cbd1e38ec2f9605942f3105e51a7d0
Interchain Adair <32375605+adairrr@users.noreply.github.com>
2024-12-16 05:53:10 -0500

    Add union-testnet-9 (#528)
    * Add union-testnet-9
    
    * Update packages/cw-orch-networks/src/networks/union.rs
    
    * Update packages/cw-orch-networks/src/networks/union.rs
    
    * Publish and changelog
    
    ---------
    
    Co-authored-by: Kayanski <kowalski.kowalskin@gmail.com>

--- packages/cw-orch-networks/src/networks/union.rs
+++ packages/cw-orch-networks/src/networks/union.rs
@@ -7,14 +7,26 @@ pub const UNION_NETWORK: NetworkInfo = NetworkInfo {
     coin_type: 118,
 };
 
-pub const UNION_TESTNET: ChainInfo = UNION_TESTNET_8;
+pub const UNION_TESTNET: ChainInfo = UNION_TESTNET_9;
 
+#[deprecated]
 pub const UNION_TESTNET_8: ChainInfo = ChainInfo {
     kind: ChainKind::Testnet,
     chain_id: "union-testnet-8",
     gas_denom: "muno",
     gas_price: 0.000025,
     grpc_urls: &["https://grpc.testnet-8.union.build:443"],
+    network_info: UNION_NETWORK,
+    lcd_url: None,
+    fcd_url: None,
+};
+
+pub const UNION_TESTNET_9: ChainInfo = ChainInfo {
+    kind: ChainKind::Testnet,
+    chain_id: "union-testnet-9",
+    gas_denom: "muno",
+    gas_price: 0.000025,
+    grpc_urls: &["https://grpc.union-testnet-9.cor.systems:443"],
     network_info: UNION_NETWORK,
     lcd_url: None,
     fcd_url: None,
